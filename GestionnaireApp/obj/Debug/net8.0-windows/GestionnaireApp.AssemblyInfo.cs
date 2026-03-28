a733631a02c51dbc3df79ef14683ac3db1f83da0
MELLOUKI FILALI Issam <issam.melloukifilali@student.hepl.be>
2026-03-28 22:18:12 +0100

    Initial Commit

--- GestionnaireApp/obj/Debug/net8.0-windows/GestionnaireApp.AssemblyInfo.cs
+++ GestionnaireApp/obj/Debug/net8.0-windows/GestionnaireApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionnaireApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63be6a587c869e5bba94e7697460ba24135c332f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionnaireApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionnaireApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
